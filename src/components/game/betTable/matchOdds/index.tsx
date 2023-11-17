11cb0e5e03b1290cab97b8eb49645b019d821786
rohanduple <150104513+rohanduple@users.noreply.github.com>
2023-11-17 11:07:52 +0530

    make different component of match odds and bookmaker

---
+++ src/components/game/betTable/matchOdds/index.tsx
@@ -0,0 +1,79 @@
+import { Table } from "react-bootstrap";
+import BackLayBox from "../../../backLayBox";
+import "../../style.scss";
+
+interface MatchOddsProps {
+  minMax?: any;
+  data: any;
+}
+function MatchOdds({ minMax, data }: MatchOddsProps) {
+  const handleClick = () => {};
+  return (
+    <div
+      className={`gameTable table-responsive sessionFancyTable borderTable border `}
+    >
+      <Table className="mb-0">
+        <thead>
+          <tr>
+            <th className="border-0">
+              {minMax && (
+                <span className="f700 title-16 px-2 text-info ">{minMax}</span>
+              )}
+            </th>
+            <th className="border-0" style={{ width: "60px" }}></th>
+            <th className="border-0" style={{ width: "60px" }}></th>
+            <th className="text-center bg-blue3" style={{ width: "60px" }}>
+              Back
+            </th>
+            <th className="text-center bg-red1" style={{ width: "60px" }}>
+              Lay
+            </th>
+            <th className="border-0" style={{ width: "60px" }}></th>
+            <th className="border-0" style={{ width: "60px" }}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item: any, i: number) => (
+            <tr key={i}>
+              <td>
+                <div className="backLayRunner d-flex flex-column px-3">
+                  <span className="backLayRunner-country title-14">
+                    {item?.RunnerName}
+                  </span>
+                  <span className="title-14">{item?.lastPriceTraded}</span>
+                </div>
+              </td>
+              {item?.ex?.availableToBack?.map((back: any, index: number) => (
+                <td key={index}>
+                  <BackLayBox
+                    style={{ width: "60px" }}
+                    // overlay={true}
+                    bgColor={`blue${index + 1}`}
+                    rate={back?.price}
+                    percent={back?.size}
+                    onClick={handleClick}
+                  />
+                </td>
+              ))}
+
+              {item?.ex?.availableToLay?.map((red: any, index: number) => (
+                <td key={index}>
+                  <BackLayBox
+                    style={{ width: "60px" }}
+                    // overlay={true}
+                    bgColor={`red${index + 1}`}
+                    rate={red?.price}
+                    percent={red?.size}
+                    onClick={handleClick}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default MatchOdds;
