70082528b7e4eb743d9199a4a55b0051e3e2c8ff
atulduple <atul.duple@gmail.com>
2023-11-22 15:38:53 +0530

    fix

--- src/utils/fieldValidations/newPassword.tsx
+++ src/utils/fieldValidations/newPassword.tsx
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 export const newPasswordValidationSchema = Yup.object({
   newPassword: Yup.string().required("Password is required"),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref("userPassword"), ""], "Passwords must match")
+    .oneOf([Yup.ref("newPassword"), ""], "Passwords must match")
     .required("Password is required"),
   transactionPassword: Yup.string().required(
     "Transaction Password is required"
