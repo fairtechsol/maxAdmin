712b022212fdcd023779bb0a758c6ca71cd8d4f6
PardeepDuple <pradeep.duple@gmail.com>
2023-12-12 15:24:12 +0530

    added changes for deposit api

--- src/components/listClients/modals/index.tsx
+++ src/components/listClients/modals/index.tsx
@@ -3,7 +3,6 @@ import ChangeStatus from "./changeStatus";
 import Credit from "./credit";
 import Deposit from "./deposit";
 import ExposureLimit from "./exposureLimit";
-import ModalFooter from "./footer";
 import Password from "./password";
 import "./style.scss";
 import Withdraw from "./withdraw";
@@ -12,18 +11,32 @@ interface ListClientModalsProps {
   id: any;
   show: boolean;
   setShow: (value: boolean) => void;
+  userData?: any;
 }
 
 const modalTitle: any = {
-  d: { name: "Deposit", item: <Deposit /> },
-  w: { name: "Withdraw", item: <Withdraw /> },
-  l: { name: "Exposure Limit", item: <ExposureLimit /> },
-  c: { name: "Credit", item: <Credit /> },
-  p: { name: "Password", item: <Password /> },
-  s: { name: "Change Status", item: <ChangeStatus /> },
+  d: { name: "Deposit", item: (props: any) => <Deposit {...props} /> },
+  w: { name: "Withdraw", item: (props: any) => <Withdraw {...props} /> },
+  l: {
+    name: "Exposure Limit",
+    item: (props: any) => <ExposureLimit {...props} />,
+  },
+  c: { name: "Credit", item: (props: any) => <Credit {...props} /> },
+  p: { name: "Password", item: (props: any) => <Password {...props} /> },
+  s: {
+    name: "Change Status",
+    item: (props: any) => <ChangeStatus {...props} />,
+  },
 };
 
-const ListClientModals = ({ id, show, setShow }: ListClientModalsProps) => {
+const ListClientModals = ({
+  id,
+  show,
+  setShow,
+  userData,
+}: ListClientModalsProps) => {
+  const { item: ModalComponent } = modalTitle[id];
+
   return (
     <CustomModal
       customClass="px-2"
@@ -31,15 +44,15 @@ const ListClientModals = ({ id, show, setShow }: ListClientModalsProps) => {
       setShow={setShow}
       title={modalTitle?.[id]?.name}
       titleStyle="fw-normal title-22"
-      footer={
-        <ModalFooter
-          clickHandler={() => {
-            setShow(false);
-          }}
-        />
-      }
+      // footer={
+      //   <ModalFooter
+      //     clickHandler={() => {
+      //       setShow(false);
+      //     }}
+      //   />
+      // }
     >
-      {modalTitle?.[id]?.item}
+      <ModalComponent userData={userData} setShow={setShow} />
     </CustomModal>
   );
 };
