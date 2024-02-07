a7cff0e9dbb34f97cacd59ca7efb4efb6b257103
PardeepDuple <pradeep.duple@gmail.com>
2024-02-07 18:36:17 +0530

    changes for current bet

--- src/pages/reports/CurrentBets/index.tsx
+++ src/pages/reports/CurrentBets/index.tsx
@@ -39,7 +39,7 @@ const CurrentBets = () => {
   const dispatch: AppDispatch = useDispatch();
   const [tableConfig, setTableConfig] = useState<TableConfig | null>(null);
   const [selectType, setSelectType] = useState({
-    value: "MATCHED",
+    value: "PENDING",
     label: "Matched",
   });
 
