de0b3a6902cb6ebc0c97615a5388b6d5c116285a
kamal9780 <kamal.dupleit@gmail.com>
2024-05-22 17:53:42 +0530

    fixed  Issues related to details screen with particular market selected from side menu

--- src/components/commonComponent/breadcrumb/index.tsx
+++ src/components/commonComponent/breadcrumb/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Breadcrumb } from "react-bootstrap";
 import "./style.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import moment from "moment-timezone";
 interface ItemObj {
   name: string;
 }
@@ -11,12 +14,15 @@ interface Props {
 }
 
 function CustomBreadcrumb({ items, style }: Props) {
+  const { matchDetails } = useSelector(
+    (state: RootState) => state.match.matchListSlice
+  );
   const inlineStyle: React.CSSProperties = {
     ...style,
   };
   return (
-    <div className="customBreadcrumb bg-secondary" style={{ ...inlineStyle }}>
-      <Breadcrumb bsPrefix="breadcrumb m-0">
+    <div className="customBreadcrumb bg-secondary d-flex justify-content-between align-items-center" style={{ ...inlineStyle }}>
+      <Breadcrumb bsPrefix="breadcrumb m-0 d-flex align-items-center">
         {items?.map((item, index) => (
           <Breadcrumb.Item
             key={index}
@@ -24,11 +30,14 @@ function CustomBreadcrumb({ items, style }: Props) {
             className="title-16 f600 p-0"
           >
             {item?.name}
-          
           </Breadcrumb.Item>
         ))}
-          <div>kwjefbdsa</div>
       </Breadcrumb>
+      <div className="title-16 f500">
+        {moment(matchDetails?.startAt).format(
+          "YYYY-MM-DD hh:mm:ss"
+        )}
+      </div>
     </div>
   );
 }
