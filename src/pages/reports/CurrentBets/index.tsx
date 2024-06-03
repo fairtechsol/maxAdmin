a08b8bdbdf7301cf05ac02348e0cfb8bd548701d
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-06-03 17:43:23 +0530

    race match starttime in current bets

--- src/pages/reports/CurrentBets/index.tsx
+++ src/pages/reports/CurrentBets/index.tsx
@@ -21,13 +21,13 @@ interface Column {
 const columns: Column[] = [
   { id: "user.userName", label: "Username" },
   { id: "eventType", label: "Event Type" },
-  { id: "match.title", label: "Event Name" },
+  { id: "eventName", label: "Event Name" },
   { id: "teamName", label: "Runner Name" },
   { id: "betType", label: "Bet Type" },
   { id: "odds", label: "User Rate" },
   { id: "amount", label: "Amount" },
   { id: "createdAt", label: "Place Date" },
-  { id: "match.startAt", label: "Match Date" },
+  { id: "startAt", label: "Match Date" },
 ];
 
 const options = [
@@ -114,7 +114,9 @@ const CurrentBets = () => {
       setRowPerPage(tableConfig?.rowPerPage);
     }
   }, [tableConfig]);
-
+  const getStartAt = (item: any) => {
+    return item?.match?.startAt || item?.racingMatch?.startAt ;
+  };
   return (
     <div className="p-2 pt-0">
       <h5 className="title-22 fw-normal">Current Bets</h5>
@@ -167,12 +169,16 @@ const CurrentBets = () => {
             <tr key={index}>
               {columns.map((column, index: number) => (
                 <td key={index}>
-                  {column?.id === "createdAt" || column?.id === "match.startAt"
-                    ? moment(_.get(item, column?.id))
-                        .tz(timezone)
-                        .format("YYYY-MMM-DD h:mmA [IST]")
-                    : _.get(item, column?.id)}
-                </td>
+                {column?.id === "createdAt"
+                  ? moment(_.get(item, column?.id))
+                      .tz(timezone)
+                      .format("YYYY-MMM-DD h:mmA [IST]")
+                  : column?.id === "startAt"
+                  ? moment(getStartAt(item))
+                      .tz(timezone)
+                      .format("YYYY-MMM-DD h:mmA [IST]")
+                  : _.get(item, column?.id)}
+              </td>
               ))}
             </tr>
           ))}
