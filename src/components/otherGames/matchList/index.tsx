8cede46c033e948fe8c978ef5d44324170fe5d0a
PardeepDuple <pradeep.duple@gmail.com>
2024-06-08 17:04:44 +0530

    fixed sorting on nav markets

--- src/components/otherGames/matchList/index.tsx
+++ src/components/otherGames/matchList/index.tsx
@@ -19,7 +19,7 @@ const NavComponent = ({ matchDetail }: any) => {
             }))
           );
         } else {
-          if (marketValue?.id) {
+          if (marketValue?.id && marketType !== "bookmaker") {
             formattedArray.push({
               type: marketValue?.type,
               id: marketValue?.id,
@@ -35,22 +35,45 @@ const NavComponent = ({ matchDetail }: any) => {
 
   const navItems: any = formatMarkets(matchDetail);
 
+  const handleSort = (a: any, b: any) => {
+    const extractParts = (type: any) => {
+      const match = type.match(/^([a-zA-Z]+)(\d*\.\d+|\d+)?$/);
+      const textPart = match ? match[1] : type;
+      const numberPart = match && match[2] ? parseFloat(match[2]) : null;
+      return { textPart, numberPart };
+    };
+
+    const aParts = extractParts(a.type);
+    const bParts = extractParts(b.type);
+
+    if (aParts.textPart < bParts.textPart) return -1;
+    if (aParts.textPart > bParts.textPart) return 1;
+
+    if (aParts.numberPart === null && bParts.numberPart === null) return 0;
+    if (aParts.numberPart === null) return -1;
+    if (bParts.numberPart === null) return 1;
+    return aParts.numberPart - bParts.numberPart;
+  };
+
   return (
     <div className="row">
       <div className="col-md-12">
         <ul className="nav mb-3">
-          {navItems.map((item: any) => (
-            <li key={item.id} className="nav-items">
-              <NavLink
-                to={`/admin/other_match_detail/${matchDetail?.id}/${item?.id}`}
-                className={`market-tab-link ${
-                  item?.id === marketId ? "active" : ""
-                }`}
-              >
-                {item.name}
-              </NavLink>
-            </li>
-          ))}
+          {navItems
+            ?.slice()
+            ?.sort(handleSort)
+            ?.map((item: any) => (
+              <li key={item.id} className="nav-items">
+                <NavLink
+                  to={`/admin/other_match_detail/${matchDetail?.id}/${item?.id}`}
+                  className={`market-tab-link ${
+                    item?.id === marketId ? "active" : ""
+                  }`}
+                >
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
