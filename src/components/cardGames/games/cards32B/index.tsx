89570c409022e187cd8e87f65e2b584d4525bbc3
PardeepDuple <pradeep.duple@gmail.com>
2024-07-22 18:03:19 +0530

    fixed matchid error

--- src/components/cardGames/games/cards32B/index.tsx
+++ src/components/cardGames/games/cards32B/index.tsx
@@ -151,7 +151,7 @@ const Card32BComponent = () => {
               }}
             >
               <Col md={12}>
-                <UserBets />
+                <UserBets matchId={dragonTigerDetail?.id} />
               </Col>
             </Row>
           </Container>
