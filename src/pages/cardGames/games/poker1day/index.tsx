5d9249505d9a796a73c1ac7bbd07d20d5f004efc
PardeepDuple <pradeep.duple@gmail.com>
2024-08-01 12:48:23 +0530

    fix for bet place in poker game

--- src/pages/cardGames/games/poker1day/index.tsx
+++ src/pages/cardGames/games/poker1day/index.tsx
@@ -34,7 +34,7 @@ const Poker1day = () => {
 
   const handleBetPlacedOnDT20 = (event: any) => {
     if (event?.jobData?.matchType === cardGamesType.poker1Day) {
-      dispatch(updateBetsPlaced(event?.jobData?.newBet));
+      dispatch(updateBetsPlaced(event?.jobData));
       dispatch(updateBalanceOnBetPlaceCards(event?.jobData));
       dispatch(updateProfitLossCards(event?.userRedisObj));
     }
