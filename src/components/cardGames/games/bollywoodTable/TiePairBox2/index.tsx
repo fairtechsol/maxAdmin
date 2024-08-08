7f3c7899a4a0c9f654b20e92ba8aff1bf8ac02a9
PardeepDuple <pradeep.duple@gmail.com>
2024-08-08 17:44:26 +0530

    updates for bollywood casino

--- src/components/cardGames/games/bollywoodTable/TiePairBox2/index.tsx
+++ src/components/cardGames/games/bollywoodTable/TiePairBox2/index.tsx
@@ -7,6 +7,23 @@ const TiePairBox2 = ({ lowHigh, data }: any) => {
   const min = lowHigh?.min;
   const max = lowHigh?.max;
   const [modelOpen, setModelOpen] = useState(false);
+
+  const getProfitLoss = (gameName: string, sid: any) => {
+    try {
+      let result = 0;
+      if (data?.profitLoss && Object.keys(data.profitLoss).length > 0) {
+        const key = `${data.videoInfo.mid}_${sid}_card`;
+        if (key in data.profitLoss) {
+          const jsonString = data.profitLoss[key];
+          const parsedData = JSON.parse(jsonString);
+          result = parsedData[gameName] ? parsedData[gameName] : 0;
+        } else return result;
+      }
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="tiePairContainer">
       <div style={{ width: "98%", textAlign: "end" }}>
@@ -29,11 +46,7 @@ const TiePairBox2 = ({ lowHigh, data }: any) => {
           value1={lowHigh?.b1}
           value4={lowHigh?.l1}
           value2={lowHigh?.nat}
-          value3={
-            data?.profitLoss
-              ? data?.profitLoss[`${data?.videoInfo?.mid}_${lowHigh?.sid}_card`]
-              : 0
-          }
+          value3={getProfitLoss("odd", lowHigh?.sid)}
           width={"100%"}
           lock={
             lowHigh?.gstatus === "CLOSED" || lowHigh?.gstatus === "SUSPENDED"
