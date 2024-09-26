43e0e22e44f768783e251ea36710e497860bf29d
PardeepDuple <pradeep.duple@gmail.com>
2024-09-26 11:04:11 +0530

    formatting

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -225,7 +225,7 @@ const Games = () => {
       console.log(error);
     }
   }, []);
-  console.log(matchDetails);
+
   return (
     <div className="gamePage">
       <Container fluid>
@@ -342,9 +342,9 @@ const Games = () => {
                             </Col>
                           );
                         })}
-                         {matchDetails?.apiSessionActive &&
-                      matchDetails?.updatedSessionBettings?.cricketCasino?.section
-                        ?.map((item: any) => {
+                    {matchDetails?.apiSessionActive &&
+                      matchDetails?.updatedSessionBettings?.cricketCasino?.section?.map(
+                        (item: any) => {
                           return (
                             <Col md={12}>
                               <BetTable
@@ -354,7 +354,8 @@ const Games = () => {
                               />
                             </Col>
                           );
-                        })}
+                        }
+                      )}
                     {/* <BetTableHeader title="runners" />
                     <div className="game-heading"><span className="card-header-title">SSD Bari v Ternana</span> <span className="float-right">5/17/2024 12:00:00 AM</span></div> */}
                     {matchDetails?.manualSessionActive &&
