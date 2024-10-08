020bf545dcc9285498174a3928e034a783ae4cb0
PardeepDuple <pradeep.duple@gmail.com>
2024-10-08 10:18:11 +0530

    fixed football match market profit loss and error handling

--- src/components/game/userBet/modal/form.tsx
+++ src/components/game/userBet/modal/form.tsx
@@ -4,7 +4,7 @@ import CustomInput from "../../../commonComponent/input";
 import { useEffect, useState } from "react";
 // import { debounce } from "lodash";
 import { useDispatch } from "react-redux";
-import { AppDispatch} from "../../../../store/store";
+import { AppDispatch } from "../../../../store/store";
 // import { searchList } from "../../../../store/actions/user/userActions";
 import { getMorePlacedBets } from "../../../../store/actions/match/matchAction";
 import CustomButton from "../../../commonComponent/button";
@@ -111,17 +111,23 @@ const UserBetModalForm = (props: any) => {
   //   }
   // }, [searchListData]);
   useEffect(() => {
-    const filtered = props.morePlacedBets.filter((item:any) => item?.deleteReason === null);
+    const filtered = props?.morePlacedBets?.filter(
+      (item: any) => item?.deleteReason === null
+    );
     setFilteredItems(filtered);
   }, []);
 
-  const handleDelete=(type:any)=>{
-    if(type==="delete"){
-      const filtered = props.morePlacedBets.filter((item:any) => item?.deleteReason != null);
+  const handleDelete = (type: any) => {
+    if (type === "delete") {
+      const filtered = props?.morePlacedBets?.filter(
+        (item: any) => item?.deleteReason != null
+      );
       setFilteredItems(filtered);
       setSelectedUser(false);
-    }else{
-      const filtered = props.morePlacedBets.filter((item:any) => item?.deleteReason === null);
+    } else {
+      const filtered = props?.morePlacedBets?.filter(
+        (item: any) => item?.deleteReason === null
+      );
       setFilteredItems(filtered);
       setSelectedUser(true);
     }
@@ -163,15 +169,19 @@ const UserBetModalForm = (props: any) => {
     }
   }, []);
 
-  const handleRadioChange=(type:any)=>{
+  const handleRadioChange = (type: any) => {
     setInputValue(type);
-    if(type==="back"){
-      const filtered = props.morePlacedBets.filter((item:any) => item?.betType === "BACK"||item?.betType === "back");
+    if (type === "back") {
+      const filtered = props?.morePlacedBets?.filter(
+        (item: any) => item?.betType === "BACK" || item?.betType === "back"
+      );
       setFilteredItems(filtered);
-    }else if(type==="lay"){
-      const filtered = props.morePlacedBets.filter((item:any) => item?.betType === "LAY"||item?.betType === "lay");
+    } else if (type === "lay") {
+      const filtered = props?.morePlacedBets?.filter(
+        (item: any) => item?.betType === "LAY" || item?.betType === "lay"
+      );
       setFilteredItems(filtered);
-    }else{
+    } else {
       setFilteredItems(props.morePlacedBets);
     }
   };
@@ -181,87 +191,117 @@ const UserBetModalForm = (props: any) => {
       className={`UserBetModalForm ${props.customClass} `}
       onSubmit={handleSubmit}
     >
-      <div className="d-flex flex-row w-100 align-items-start p-2" style={{backgroundColor:"#f6f6f6"}}>
-       <button style={{fontSize:"14px",padding:"6px",backgroundColor:selectedUser?"#556ee6":"#eee",border:0,color:selectedUser?"#fff":"#333",borderRadius:"5px"}} onClick={()=>handleDelete("match")}>Matched Bets</button>
-       <button style={{fontSize:"14px",padding:"6px",backgroundColor:selectedUser?"#eee":"#556ee6",border:0,color:selectedUser?"#333":"#fff",borderRadius:"5px"}} onClick={()=>handleDelete("delete")}>Deleted Bets</button>
+      <div
+        className="d-flex flex-row w-100 align-items-start p-2"
+        style={{ backgroundColor: "#f6f6f6" }}
+      >
+        <button
+          style={{
+            fontSize: "14px",
+            padding: "6px",
+            backgroundColor: selectedUser ? "#556ee6" : "#eee",
+            border: 0,
+            color: selectedUser ? "#fff" : "#333",
+            borderRadius: "5px",
+          }}
+          onClick={() => handleDelete("match")}
+        >
+          Matched Bets
+        </button>
+        <button
+          style={{
+            fontSize: "14px",
+            padding: "6px",
+            backgroundColor: selectedUser ? "#eee" : "#556ee6",
+            border: 0,
+            color: selectedUser ? "#333" : "#fff",
+            borderRadius: "5px",
+          }}
+          onClick={() => handleDelete("delete")}
+        >
+          Deleted Bets
+        </button>
       </div>
-      <div className="d-flex flex-column p-1" style={{border:"1px solid #b6b4b4"}}>
-      <div className="row">
-        <div className="d-flex flex-row">
-          <div className="flex-column d-flex align-items-start">
-            <div className="col-md-12">
-              <Form.Label>Usename</Form.Label>
+      <div
+        className="d-flex flex-column p-1"
+        style={{ border: "1px solid #b6b4b4" }}
+      >
+        <div className="row">
+          <div className="d-flex flex-row">
+            <div className="flex-column d-flex align-items-start">
+              <div className="col-md-12">
+                <Form.Label>Usename</Form.Label>
+              </div>
+              <div className="col-md-12">
+                <CustomInput
+                  placeholder="Search Username"
+                  type="text"
+                  name="userName"
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
-            <div className="col-md-12">
-              <CustomInput
-                placeholder="Search Username"
-                type="text"
-                name="userName"
-                onChange={handleInputChange}
-              />
+            <div className="flex-column d-flex align-items-start me-2 ms-2">
+              <div className="col-md-12">
+                <Form.Label>IP Address</Form.Label>
+              </div>
+              <div className="col-md-12">
+                <CustomInput
+                  placeholder="Enter IP Address"
+                  type="text"
+                  name="ipAddress"
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex-column d-flex align-items-start me-2 ms-2">
-            <div className="col-md-12">
-              <Form.Label>IP Address</Form.Label>
-            </div>
-            <div className="col-md-12">
-              <CustomInput
-                placeholder="Enter IP Address"
-                type="text"
-                name="ipAddress"
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="flex-column d-flex align-items-start">
-            <div className="col-md-12">
-              <Form.Label>Amount From</Form.Label>
-            </div>
-            <div className="col-md-12">
-              <div className="d-flex">
-                <div className="me-2">
-                  <CustomInput
-                    // customstyle=" w-50"
-                    type="number"
-                    name="minAmount"
-                    placeholder="Amount From"
-                    value={inputFields.minAmount}
-                    onChange={handleInputChange}
-                  />
+            <div className="flex-column d-flex align-items-start">
+              <div className="col-md-12">
+                <Form.Label>Amount From</Form.Label>
+              </div>
+              <div className="col-md-12">
+                <div className="d-flex">
+                  <div className="me-2">
+                    <CustomInput
+                      // customstyle=" w-50"
+                      type="number"
+                      name="minAmount"
+                      placeholder="Amount From"
+                      value={inputFields.minAmount}
+                      onChange={handleInputChange}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex-column d-flex align-items-start">
-            <div className="col-md-12">
-              <Form.Label>Amount To</Form.Label>
-            </div>
-            <div className="col-md-12">
-              <div className="d-flex">
-                <div className="me-2">
-                  <CustomInput
-                    // customstyle="w-50"
-                    type="number"
-                    name="maxAmount"
-                    placeholder="Amount To"
-                    value={inputFields.maxAmount}
-                    onChange={handleInputChange}
-                  />
+            <div className="flex-column d-flex align-items-start">
+              <div className="col-md-12">
+                <Form.Label>Amount To</Form.Label>
+              </div>
+              <div className="col-md-12">
+                <div className="d-flex">
+                  <div className="me-2">
+                    <CustomInput
+                      // customstyle="w-50"
+                      type="number"
+                      name="maxAmount"
+                      placeholder="Amount To"
+                      value={inputFields.maxAmount}
+                      onChange={handleInputChange}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="d-flex align-items-end">
+              <CustomButton type="submit">Submit</CustomButton>
+            </div>
+            <div className="d-flex align-items-end ms-1">
+              <CustomButton type="reset">Reset</CustomButton>
+            </div>
           </div>
-          <div className="d-flex align-items-end">
-            <CustomButton type="submit">Submit</CustomButton>
-          </div>
-          <div className="d-flex align-items-end ms-1">
-            <CustomButton type="reset">Reset</CustomButton>
-          </div>
-        </div>
 
-        <div className="col-lg-12">
-          {/* <div className="flex-column d-flex align-items-start">
+          <div className="col-lg-12">
+            {/* <div className="flex-column d-flex align-items-start">
             <div className="col-md-3">
               <Form.Label>Amount</Form.Label>
             </div>
@@ -289,7 +329,7 @@ const UserBetModalForm = (props: any) => {
             </div>
           </div> */}
 
-          {/* <div className="col-md-3">
+            {/* <div className="col-md-3">
               <Form.Group>Bet Type</Form.Group>
             </div>
             <div className="col-md-3">
@@ -303,48 +343,47 @@ const UserBetModalForm = (props: any) => {
                 onChange={handleChange}
               />
             </div> */}
-          <div className="d-flex flex-row justify-content-between w-100 mt-2">
-            <div className="d-flex flex-row justify-content-around align-items-center w-20">
-              <div className="d-flex flex-row justify-content-around align-items-center w-25">
-                <input
-                  type="radio"
-                  name="betType"
-                  id={inputValue}
-                  onChange={()=>handleRadioChange("all")}
-                  defaultChecked 
-                />
-                <span>All</span>
-              </div>
+            <div className="d-flex flex-row justify-content-between w-100 mt-2">
+              <div className="d-flex flex-row justify-content-around align-items-center w-20">
+                <div className="d-flex flex-row justify-content-around align-items-center w-25">
+                  <input
+                    type="radio"
+                    name="betType"
+                    id={inputValue}
+                    onChange={() => handleRadioChange("all")}
+                    defaultChecked
+                  />
+                  <span>All</span>
+                </div>
 
-              <div className="d-flex flex-row justify-content-around align-items-center w-25">
-                <input
-                  type="radio"
-                  name="betType"
-                  id={inputValue}
-                  onChange={()=>handleRadioChange("back")}
-                />
-                <span>Back</span>
+                <div className="d-flex flex-row justify-content-around align-items-center w-25">
+                  <input
+                    type="radio"
+                    name="betType"
+                    id={inputValue}
+                    onChange={() => handleRadioChange("back")}
+                  />
+                  <span>Back</span>
+                </div>
+                <div className="d-flex flex-row justify-content-around align-items-center w-25">
+                  <input
+                    type="radio"
+                    name="betType"
+                    id={inputValue}
+                    onChange={() => handleRadioChange("lay")}
+                  />
+                  <span>Lay</span>
+                </div>
               </div>
-              <div className="d-flex flex-row justify-content-around align-items-center w-25">
-                <input
-                  type="radio"
-                  name="betType"
-                  id={inputValue}
-                  onChange={()=>handleRadioChange("lay")}
-                />
-                <span>Lay</span>
+              <div className="d-flex flex-row justify-content-around align-items-center w-20">
+                <span>Total SODA : 1</span>
+                <span>Total Amount : 100.00</span>
               </div>
-            </div>
-            <div className="d-flex flex-row justify-content-around align-items-center w-20">
-              <span>Total SODA : 1</span>
-              <span>Total Amount : 100.00</span>
             </div>
           </div>
         </div>
+        <UserBetModalTable list={filteredItems} />
       </div>
-      <UserBetModalTable list={filteredItems}/>
-      </div>
-      
     </form>
   );
 };
