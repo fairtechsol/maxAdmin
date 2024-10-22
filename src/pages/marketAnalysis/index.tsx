e5454058bebad36d50960387f2743743b9ae3762
PardeepDuple <pradeep.duple@gmail.com>
2024-10-22 16:10:46 +0530

    updates for market analysis list

--- src/pages/marketAnalysis/index.tsx
+++ src/pages/marketAnalysis/index.tsx
@@ -1,14 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
 import MarketAnalysisComp from "../../components/marketAnalysis";
+import { AppDispatch, RootState } from "../../store/store";
+import { useEffect } from "react";
+import { getMarketAnalysis } from "../../store/actions/match/matchAction";
+import { FaSync } from "react-icons/fa";
 
 const MarketAnalysis = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  const { marketAnalysisDetail } = useSelector(
+    (state: RootState) => state.match.marketAnalysis
+  );
+
+  useEffect(() => {
+    dispatch(getMarketAnalysis({}));
+  }, []);
   return (
     <>
       <div className="px-3">
-        <h3 className="fw-normal title-22">Market Analysis</h3>
-        <p className="mb-0 title-14">
-          You can view your cricket card books from sport menu.
-        </p>
-        <MarketAnalysisComp/>
+        <h3 className="fw-normal title-22">
+          Market Analysis{" "}
+          <FaSync
+            onClick={() => dispatch(getMarketAnalysis({}))}
+            size={15}
+            cursor={"pointer"}
+          />
+        </h3>
+        {marketAnalysisDetail?.map((match: any) => (
+          <MarketAnalysisComp match={match} />
+        ))}
       </div>
     </>
   );
