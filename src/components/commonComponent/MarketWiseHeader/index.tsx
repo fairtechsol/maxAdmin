73f3b4241d488a50ac8a96652a9c32a0490f76c8
AkashDuple <160487146+AkashDuple@users.noreply.github.com>
2024-12-17 16:21:23 +0530

    user-book

--- src/components/commonComponent/MarketWiseHeader/index.tsx
+++ src/components/commonComponent/MarketWiseHeader/index.tsx
@@ -77,8 +77,8 @@ function MarketTableHeader({
   let columns = [
     { id: "userName", label: "User Name" },
     //{ id: detail?.teamA, label: detail?.teamA },
-    { id: detail?.teamB, label: detail?.teamB },
-    ...(detail?.teamC ? [{ id: detail.teamC, label: detail.teamC }] : []),
+    //{ id: detail?.teamB, label: detail?.teamB },
+    //...(detail?.teamC ? [{ id: detail.teamC, label: detail.teamC }] : []),
     ...(mappedNats ? mappedNats : []),
   ];
 
@@ -230,7 +230,7 @@ function MarketTableHeader({
     // }, 1000);
   };
 
-  
+  console.log("data",detail);
  
   return (
     <>
